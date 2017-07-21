2d6eb71eee71f0469afd85168a84113b70955fd6
nebulae12 <nebulae12@gmail.com>
2017-07-21 02:26:26 -0400

    THE BUTTON WORKS AHH

--- JavaScript/nebulous_app.js
+++ JavaScript/nebulous_app.js
@@ -1,1 +1,17 @@
-//Javascript here!
+upArrow = document.querySelector("#backToTop");
+
+//Enable the scroll-to-top arrow button when user scrolls down enough
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    upArrow.style.display = "block";
+  }
+}
+
+//Clicking the button scrolls the user to the top of the page
+upArrow.addEventListener("click", () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+);
